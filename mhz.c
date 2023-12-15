wtarreau/mhz

commit 6ec38cbb1371d20078a7a5059dd9faa5b281b2d9

mhz: shorten the calibration delay
By halving the expected loop time we can increase the number of long
loops from 3 to 5 and further improve the measurement accuracy by an
extra zero, which gives a stability of 0.0017% on a core i7 at 4.4 GHz.

mhz.c
@@ -131,7 +131,7 @@ void run_once(long count)
 	 * among 3 runs of the short loop.
 	 */
 	us_end250 = LLONG_MAX;
-	for (i = 0; i < 3; i++) {
+	for (i = 0; i < 5; i++) {
 		us_begin   = microseconds();
 		tsc_begin  = rdtsc();
 		loop250(count);
@@ -173,7 +173,7 @@ unsigned int calibrate()
 	unsigned long long start;
 	unsigned int count = 1000;
 
-	while (duration < 20000) {
+	while (duration < 10000) {
 		count = count * 5 / 4;
 		start = microseconds();
 		loop50(count);

commit a9b78236e57d600aeb99e47963dff5e4a3ddb7ae

mhz: fix long loops comment
The comment for the long loop incorrectly refers to the short loop, and
still mentions the number of iterations before it was increased.

Fixes: 6ec38cbb1371d200 ("mhz: shorten the calibration delay")
Fixes: 6620e45f41429afe ("mhz: improve measurement accuracy")
Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>

mhz.c
@@ -135,7 +135,7 @@ void run_once(long count)
 	}
 
 	/* now run the 250 cycles loop. We'll pick the lowest value
-	 * among 3 runs of the short loop.
+	 * among 5 runs of the long loop.
 	 */
 	us_end250 = LLONG_MAX;
 	for (i = 0; i < 5; i++) {